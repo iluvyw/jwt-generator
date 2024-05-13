384d77db04b090ff53bc4d096ff847734ba18e97
anpham <anhoangpham2001@gmail.com>
2024-05-13 18:53:25 +0700

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use sha2::Sha256;
 
 const SECRET_KEY: &str = "secret";
 
+fn encode<T: AsRef<[u8]>>(input: T) -> String {
+    BASE64_URL_SAFE.encode(input).replace('=', "")
+}
+
 fn main() {
     let header = json!({
         "type": "JWT",
@@ -15,15 +19,15 @@ fn main() {
         "role": "dev"
     });
 
-    let encoded_header = BASE64_URL_SAFE.encode(&header.to_string()).replace('=', "");
-    let encoded_payload = BASE64_URL_SAFE.encode(&payload.to_string()).replace('=', "");
+    let encoded_header = encode(&header.to_string());
+    let encoded_payload = encode(&payload.to_string());
     let token_data = format!("{}.{}", encoded_header, encoded_payload);
     
     type HmacSha256 = Hmac<Sha256>;
     let mut mac = HmacSha256::new_from_slice(SECRET_KEY.as_bytes()).expect("HMAC can take key of any size");
     mac.update(token_data.as_bytes());
     let result = mac.finalize();
-    let signature: String = BASE64_URL_SAFE.encode(result.into_bytes()).replace('=', "");
+    let signature: String = encode(result.into_bytes());
     let jwt = format!("{}.{}", token_data, &signature);
     
     println!("Your token is {}", &jwt);
